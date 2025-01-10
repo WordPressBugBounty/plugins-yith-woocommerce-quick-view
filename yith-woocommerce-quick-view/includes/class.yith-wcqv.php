ae18a3f286355695aae4636bf9cf864f2fc80aa4
Sean Murphy <sean@iamseanmurphy.com>
2025-01-10 14:17:11 +0000

    1.43.0

--- yith-woocommerce-quick-view/includes/class.yith-wcqv.php
+++ yith-woocommerce-quick-view/includes/class.yith-wcqv.php
@@ -1,4 +1,4 @@
-<?php //phpcs:ignore WordPress.Files.FileName.InvalidClassFileName
+<?php
 /**
  * Main class
  *
@@ -63,6 +63,9 @@ if ( ! class_exists( 'YITH_WCQV' ) ) {
 		 */
 		public function __construct() {
 
+			// Load Plugin Framework.
+			add_action( 'after_setup_theme', array( $this, 'plugin_fw_loader' ), 1 );
+
 			if ( $this->can_load() ) {
 				if ( $this->is_admin() ) {
 					require_once 'class.yith-wcqv-admin.php';
@@ -73,8 +76,6 @@ if ( ! class_exists( 'YITH_WCQV' ) ) {
 					YITH_WCQV_Frontend();
 				}
 			}
-
-			add_action( 'init', array( $this, 'add_image_size' ) );
 		}
 
 		/**
@@ -121,24 +122,29 @@ if ( ! class_exists( 'YITH_WCQV' ) ) {
 		 * @return boolean
 		 */
 		public function load_frontend() {
+			$enable           = get_option( 'yith-wcqv-enable', 'yes' ) === 'yes';
 			$enable_on_mobile = get_option( 'yith-wcqv-enable-mobile', 'yes' ) === 'yes';
-			return apply_filters( 'yith_wcqv_load_frontend', ! wp_is_mobile() || $enable_on_mobile );
+			$is_mobile        = wp_is_mobile();
+
+			return apply_filters( 'yith_wcqv_load_frontend', ( ! $is_mobile && $enable ) || ( $is_mobile && $enable_on_mobile ) );
 		}
 
 
 		/**
-		 * Add image size
+		 * Load Plugin Framework
 		 *
-		 * @since  2.0.0
+		 * @since  1.0
+		 * @access public
 		 * @return void
 		 */
-		public function add_image_size() {
-			// Set image size.
-			$dimensions = get_option('yith-quick-view-product-image-dimensions', array('dimensions' => array( 'width' => 450, 'height' => 600, ), 'unit' => 'px', 'linked' => 'no',));
-			$width = $dimensions['dimensions']['width'] ?? 450;
-			$height = $dimensions['dimensions']['height'] ?? 600;
-
-			add_image_size( 'quick_view_image_size', $width, $height, true );
+		public function plugin_fw_loader() {
+			if ( ! defined( 'YIT_CORE_PLUGIN' ) ) {
+				global $plugin_fw_data;
+				if ( ! empty( $plugin_fw_data ) ) {
+					$plugin_fw_file = array_shift( $plugin_fw_data );
+					require_once $plugin_fw_file;
+				}
+			}
 		}
 	}
 }
--- yith-woocommerce-quick-view/plugin-options/settings-options.php
+++ yith-woocommerce-quick-view/plugin-options/settings-options.php
@@ -20,10 +20,18 @@ $settings = array(
 			'id'    => 'yith-wcqv-general-options',
 		),
 
+		'enable-quick-view'        => array(
+			'id'        => 'yith-wcqv-enable',
+			'name'      => __( 'Enable Quick View', 'yith-woocommerce-quick-view' ),
+			'type'      => 'yith-field',
+			'yith-type' => 'onoff',
+			'default'   => 'yes',
+		),
+
 		'enable-quick-view-mobile' => array(
 			'id'        => 'yith-wcqv-enable-mobile',
 			'name'      => __( 'Enable Quick View on mobile', 'yith-woocommerce-quick-view' ),
-			'desc'      => __( 'Enable quick view features for mobile devices.', 'yith-woocommerce-quick-view' ),
+			'desc'      => __( 'Enable quick view features on mobile device too', 'yith-woocommerce-quick-view' ),
 			'type'      => 'yith-field',
 			'yith-type' => 'onoff',
 			'default'   => 'yes',
@@ -31,8 +39,8 @@ $settings = array(
 
 		'quick-view-label'         => array(
 			'id'        => 'yith-wcqv-button-label',
-			'name'      => __( '"Quick view" button label', 'yith-woocommerce-quick-view' ),
-			'desc'      => __( '"Quick view" button label in the WooCommerce loop.', 'yith-woocommerce-quick-view' ),
+			'name'      => __( 'Quick View Button Label', 'yith-woocommerce-quick-view' ),
+			'desc'      => __( 'Label for the quick view button in the WooCommerce loop.', 'yith-woocommerce-quick-view' ),
 			'type'      => 'yith-field',
 			'yith-type' => 'text',
 			'default'   => __( 'Quick View', 'yith-woocommerce-quick-view' ),
@@ -43,49 +51,38 @@ $settings = array(
 			'id'   => 'yith-wcqv-general-options',
 		),
 
-		'content-options' => array(
-			'title' => __( 'Content Options', 'yith-woocommerce-quick-view' ),
-			'type'  => 'title',
+		'style-options'            => array(
+			'title' => __( 'Style Options', 'yith-woocommerce-quick-view' ),
 			'desc'  => '',
-			'id'    => 'yith-wcqv-content-options',
+			'type'  => 'title',
+			'id'    => 'yith-wcqv-style-options',
 		),
 
-		'product-description'  => array(
-			'id'        => 'yith-wcqv-product-description',
-			'name'      => __( 'Product description:', 'yith-woocommerce-quick-view' ),
+		'background-color-modal'   => array(
+			'name'      => __( 'Modal Window Background Color', 'yith-woocommerce-quick-view' ),
 			'type'      => 'yith-field',
-			'yith-type' => 'radio',
-			'default'   => 'short',
-			'options'   => array(
-				'short' => __( 'Show short description', 'yith-woocommerce-quick-view' ),
-				'full'  => __( 'Show full description', 'yith-woocommerce-quick-view' ),
-			),
+			'yith-type' => 'colorpicker',
+			'desc'      => '',
+			'id'        => 'yith-wcqv-background-modal',
+			'default'   => '#ffffff',
 		),
 
-		'image-option' => array(
-			'id'         => 'yith-quick-view-product-image-dimensions',
-			'title'      => __( 'Product image dimensions', 'yith-woocommerce-quick-view' ),
-			'desc'       => sprintf( _x( 'Set the image size (in px). After changing these settings you may need to %sregenerate your thumbnails%s.', 'placeholders are html tags', 'yith-woocommerce-quick-view' ), '<a href="https://wordpress.org/plugins/regenerate-thumbnails/">', '</a>' ),
-			'type'       => 'yith-field',
-			'yith-type'  => 'dimensions',
-			'dimensions' => array(
-				'width'  => __( 'Width', 'yith-woocommerce-quick-view' ),
-				'height' => __( 'Height', 'yith-woocommerce-quick-view' ),
-			),
-			'units'      => array( 'px' => 'px' ),
-			'default'    => array(
-				'dimensions' => array(
-					'width'  => 450,
-					'height' => 600,
-				),
-				'unit'       => 'px',
-				'linked'     => 'no',
-			),
-			'min'        => 1,
-			'deps'       => array(
-				'id'    => 'yith-wcqv-product-show-thumb',
-				'value' => 'yes',
-			),
+		'close-button-color'       => array(
+			'name'      => __( 'Closing Button Color', 'yith-woocommerce-quick-view' ),
+			'type'      => 'yith-field',
+			'yith-type' => 'colorpicker',
+			'desc'      => '',
+			'id'        => 'yith-wcqv-close-color',
+			'default'   => '#cdcdcd',
+		),
+
+		'close-button-color-hover' => array(
+			'name'      => __( 'Closing Button Hover Color', 'yith-woocommerce-quick-view' ),
+			'type'      => 'yith-field',
+			'yith-type' => 'colorpicker',
+			'desc'      => '',
+			'id'        => 'yith-wcqv-close-color-hover',
+			'default'   => '#ff0000',
 		),
 
 		'style-options-end'        => array(
